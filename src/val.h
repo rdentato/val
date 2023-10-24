36401714ad848a0cbd5a40eccfabd3a90f61948b
rdentato <rdentato@gmail.com>
2023-10-24 09:07:43 +0200

    refactoring.

--- src/val.h
+++ src/val.h
@@ -1,4 +1,4 @@
-//  (C) by Remo Dentato (rdentato@gmail.com)
+//  SPDX-FileCopyrightText: © 2023 Remo Dentato <rdentato@gmail.com>
 //  SPDX-License-Identifier: MIT
 //  PackageVersion: 0.1.0 Beta
 
@@ -26,33 +26,33 @@
 // (see table 4.3 on pages 90)
 
 //                  ╭─── Quiet NaN
-//                  │╭── QNaN Floating-Point Indefinites have 0 here
+//                  │╭── QNaN Floating-Point Indefinites has 0 fron here up to the end
 //                  ▼▼ 
-//   x111 1111 1111 11xx FF FF FF FF FF FF
-//    ╰──────┬───────╯   ╰───────┬───────╯
+//   x111 1111 1111 1xxx FF FF FF FF FF FF
+//    ╰──────┬──────╯    ╰───────┬───────╯
 //           │                   │ 
-//    11 bits set to 1   Payload (48 bits)
-//          7FFC
+//    12 bits set to 1   Payload (48 bits)
+//          7FF8
 
 typedef struct {uint64_t v;} val_t;
- 
+typedef struct {char *s;} chs_t;
 typedef struct vec_s *vec_t;
 
 // Some bitmask
 #define VAL_TYPE_MASK ((uint64_t)0xFFFF000000000000)
-#define VAL_NAN_MASK  ((uint64_t)0x7FFC000000000000)
+#define VAL_NAN_MASK  ((uint64_t)0x7FF0000000000000)
 #define VAL_PAYLOAD   ((uint64_t)0x0000FFFFFFFFFFFF)
 #define VAL_PTR_MASK  ((uint64_t)0xFFFD000000000000)
 #define VAL_VEC_MASK  ((uint64_t)0xFFFE000000000000)
 #define VAL_STR_MASK  ((uint64_t)0xFFFF000000000000)
 #define VAL_INT_MASK  ((uint64_t)0x7FFF000000000000)
 #define VAL_UNS_MASK  ((uint64_t)0x7FFE000000000000)
-#define VAL_BOOL_MASK ((uint64_t)0x7FFD000000000000)
+#define VAL_CST_MASK  ((uint64_t)0xFFF8FFFFFFFFFF00)
 
 // Check the type of a val_t variable
 #define valisinteger(x) ((val(x).v & (uint64_t)0xFFFE000000000000) == VAL_UNS_MASK)
 #define valissigned(x)  ((val(x).v & VAL_TYPE_MASK) == VAL_INT_MASK)
-#define valisbool(x)    ((val(x).v & (uint64_t)0xFFFFFFFFFFFFFFFE) == VAL_BOOL_MASK)
+#define valisbool(x)    ((val(x).v & (uint64_t)0xFFFFFFFFFFFFFFFE) == VAL_CST_MASK )
 #define valisdouble(x)  ((val(x).v & VAL_NAN_MASK) != VAL_NAN_MASK)
 #define valispointer(x) ((val(x).v & VAL_TYPE_MASK) == VAL_PTR_MASK)
 #define valisstring(x)  ((val(x).v & VAL_TYPE_MASK) == VAL_STR_MASK)
@@ -65,14 +65,14 @@ typedef struct vec_s *vec_t;
 
 // Store a value into a val_t variable
 static inline val_t val_fromchar(char v)             {val_t ret; ret.v = VAL_INT_MASK | ((uint64_t)v);                  return ret;}
-static inline val_t val_fromint(int v)               {val_t ret; ret.v = VAL_INT_MASK | ((uint64_t)v);                  return ret;}
-static inline val_t val_fromshort(short v)           {val_t ret; ret.v = VAL_INT_MASK | ((uint64_t)v);                  return ret;}
-static inline val_t val_fromlong(long v)             {val_t ret; ret.v = VAL_INT_MASK | ((uint64_t)(v) & VAL_PAYLOAD);  return ret;}
 static inline val_t val_fromuchar(unsigned char v)   {val_t ret; ret.v = VAL_UNS_MASK | ((uint64_t)v);                  return ret;}
+static inline val_t val_fromint(int v)               {val_t ret; ret.v = VAL_INT_MASK | ((uint64_t)v);                  return ret;}
 static inline val_t val_fromuint(unsigned int v)     {val_t ret; ret.v = VAL_UNS_MASK | ((uint64_t)v);                  return ret;}
+static inline val_t val_fromshort(short v)           {val_t ret; ret.v = VAL_INT_MASK | ((uint64_t)v);                  return ret;}
 static inline val_t val_fromushort(unsigned short v) {val_t ret; ret.v = VAL_UNS_MASK | ((uint64_t)v);                  return ret;}
+static inline val_t val_fromlong(long v)             {val_t ret; ret.v = VAL_INT_MASK | ((uint64_t)(v) & VAL_PAYLOAD);  return ret;}
 static inline val_t val_fromulong(unsigned long v)   {val_t ret; ret.v = VAL_UNS_MASK | ((uint64_t)(v) & VAL_PAYLOAD);  return ret;}
-static inline val_t val_frombool(_Bool v)            {val_t ret; ret.v = VAL_BOOL_MASK| ((uint64_t)((v) & 1));          return ret;}
+static inline val_t val_frombool(_Bool v)            {val_t ret; ret.v = VAL_CST_MASK | ((uint64_t)((v) & 1));          return ret;}
 static inline val_t val_fromptr(void *v)             {val_t ret; ret.v = VAL_PTR_MASK | ((uintptr_t)(v) & VAL_PAYLOAD); return ret;}
 static inline val_t val_fromstr(void *v)             {val_t ret; ret.v = VAL_STR_MASK | ((uintptr_t)(v) & VAL_PAYLOAD); return ret;}
 static inline val_t val_fromvec(void *v)             {val_t ret; ret.v = VAL_VEC_MASK | ((uintptr_t)(v) & VAL_PAYLOAD); return ret;}
@@ -82,7 +82,7 @@ static inline val_t val_fromfloat(float f)           {return val_fromdouble((dou
 
 static inline val_t val_fromval(val_t v)             {return v;}
 
-#define val(x) _Generic( (x), int: val_fromint,    \
+#define val_(x) _Generic((x), int: val_fromint,    \
                              char: val_fromchar,   \
                             short: val_fromshort,  \
                              long: val_fromlong,   \
@@ -99,32 +99,43 @@ static inline val_t val_fromval(val_t v)             {return v;}
                            char *: val_fromstr,    \
                            void *: val_fromptr) (x)
 
-// Retrieve a value from a val_t variable
-static inline   void *valtopointer(val_t v) { return (void *)((uintptr_t)((v.v) & VAL_PAYLOAD));}
-static inline double  valtodouble(val_t v)  { double d; memcpy(&d,&v,8); return d;}
-static inline  float  valtofloat(val_t v)   { return (float)valtodouble(v);}
+#define val(x) _Generic((x), val_t: x, default: val_(x))
 
-static inline  _Bool  valtobool(val_t v)    { return (_Bool)((v.v)&1);}
+// Retrieve a value from a val_t variable
+#define valtodouble(v) val_todouble(val(v))
+static inline   double val_todouble(val_t v)  { double d; memcpy(&d,&v,8); return d;}
+
+#define valtofloat(v) val_tofloat(val(v))
+static inline  float  val_tofloat(val_t v)    { return (float)val_todouble(v);}
+
+#define valtobool(v) val_tobool(val(v))
+static inline  _Bool val_tobool(val_t v)      { return (_Bool)((v.v)&1);}
 
 #define VAL_SIGNED_MASK ((uint64_t)0x0001800000000000)
-static inline   long  valtointeger(val_t v) { long ret = ((v.v) & VAL_PAYLOAD); 
-                                              if ( ((v.v) & VAL_SIGNED_MASK) == VAL_SIGNED_MASK) // if signed and negative
-                                                  ret |= (uint64_t)0xFFFF000000000000;          // then extend sign
-                                              return ret;
-                                            }
+#define valtointeger(x) val_tointeger(val(x))
+static inline      long val_tointeger(val_t v) \
+                                  { long ret = ((v.v) & VAL_PAYLOAD); 
+                                    if ( ((v.v) & VAL_SIGNED_MASK) == VAL_SIGNED_MASK) // if signed and negative
+                                       ret |= (uint64_t)0xFFFF000000000000;            // then extend sign
+                                    return ret;
+                                  }
 
+static inline   void *val_topointer(val_t v, uint64_t mask) { return (void *)((uintptr_t)((v.v) & mask));}
+
+#define val_to_pointer(v_, m_) ((void *)((uintptr_t)((val(v_).v) & (m_))))
+#define valtopointer(v) val_to_pointer(v,VAL_PAYLOAD)
 #define valtostring(v) ((char *)valtopointer(v))
-#define valtovec(v)    ((vec_t)valtopointer(v))
+#define valtovec(v)    ((vec_t)val_to_pointer(v,((uint64_t)0x0000FFFFFFFFFFFE)))
 
 // Some constant
-#define valfalse      ((val_t){0x7FFD000000000000})
-#define valtrue       ((val_t){0x7FFD000000000001})
-#define valnil        ((val_t){0x7FFDFFFFFFFFFFE0})
-#define valundefined  ((val_t){0x7FFDFFFFFFFFFFD0})
-#define valdeleted    ((val_t){0x7FFDFFFFFFFFFFC0})
-#define valempty      ((val_t){0x7FFDFFFFFFFFFFB0})
-#define valmarked     ((val_t){0x7FFDFFFFFFFFFFE1})
-#define valnilpointer ((val_t){0xFFFF000000000000})
+#define valfalse      ((val_t){0xFFF8FFFFFFFFFF00})
+#define valtrue       ((val_t){0xFFF8FFFFFFFFFF01})
+#define valnil        ((val_t){0xFFF80FFFFFFFFFE0})
+#define valundefined  ((val_t){0xFFF80FFFFFFFFFD0})
+#define valdeleted    ((val_t){0xFFF80FFFFFFFFFC0})
+#define valempty      ((val_t){0xFFF80FFFFFFFFFB0})
+#define valmarked     ((val_t){0xFFF80FFFFFFFFFE1})
+#define valnilpointer ((val_t){0xFFFD000000000000})
 
 #define valnilstr   val_nilstr()
 static inline val_t val_nilstr() {static char *s=""; return val_fromstr(s);}
--- src/vec.c
+++ src/vec.c
@@ -1,22 +1,28 @@
-
-//  (C) by Remo Dentato (rdentato@gmail.com)
+//  SPDX-FileCopyrightText: © 2023 Remo Dentato <rdentato@gmail.com>
 //  SPDX-License-Identifier: MIT
-//  PackageVersion: 0.4.0 Beta
 
 #include "vec.h"
 
-vec_t vecnew()
+int vecisowned(val_t vv)
 {
-  vec_t v;
-  v = calloc(1,sizeof(struct vec_s));
-  if (v == NULL) errno = ENOMEM;
-  return v;
+  if (!valisvec(vv)) return 0;
+  return ((valtovec(vv)->typ & 0x80) == 0x80);
 }
 
-vec_t vecfree(vec_t v)
+int valisvecref(val_t vv)
 {
-  free(v);
-  return NULL;
+  return (vecisowned(vv)) ? (vv.v & 1) : 0;
+}
+
+val_t vecfree(val_t vv) { 
+  vec_t v;
+  errno = 0;
+  if (valisvecref(vv)) { 
+    v = valtovec(vv); 
+    free(v->vec);
+    free(v); 
+  } else errno = EINVAL;
+  return valnil;
 }
 
 // Ensure the vector is large enough to store a value at index n
@@ -28,7 +34,7 @@ static int makeroom(vec_t v, uint32_t n)
   errno = 0;
   if (!v) {errno = EINVAL; return 0;}
 
- _vecdbg("Making room %p (%p)[%d]->[%d]",(void *)v,(void *)v->vec,v->sze,n);
+  vec_dbg("Making room %p (%p)[%d]->[%d]",(void *)v,(void *)v->vec,v->sze,n);
 
   // There should be at least n+1 slots:
   n += 1;
@@ -49,9 +55,10 @@ static int makeroom(vec_t v, uint32_t n)
   
   if (new_sze <= n) { errno = ERANGE; return 0; }
 
- _vecdbg("MKROOM: realloc(%p,%d) [%d]",v->vec,new_sze * sizeof(val_t),new_sze);
+  vec_dbg("MKROOM: realloc(%p,%d) [%d]",v->vec,new_sze * sizeof(val_t),new_sze);
   new_vec = realloc(v->vec, new_sze * sizeof(val_t));
- _vecdbg("MKROOM: got(%p,%d) [%d]",new_vec,new_sze * sizeof(val_t),new_sze);
+  assert(((uintptr_t)new_vec & 1) == 0); // assume that all malloced pointers are at least even-aligned
+  vec_dbg("MKROOM: got(%p,%d) [%d]",new_vec,new_sze * sizeof(val_t),new_sze);
 
   if (new_vec == NULL) { errno = ENOMEM; return 0; }
   
@@ -62,8 +69,87 @@ static int makeroom(vec_t v, uint32_t n)
 }
 
 
-uint32_t vec_set(vec_t v, uint32_t i, val_t x)
+int vec_gap_3(val_t vv, uint32_t i, uint32_t l)
 {
+  vec_t v;
+  int n;
+
+  if (!valisvec(vv)) {errno = EINVAL; return 0;}
+  v = valtovec(vv);
+
+  if (i == VECNONDX) i = v->cnt;
+  if (l == VECNONDX) l = 1;
+  vecdbg("GAP : %d %d",i,l);
+
+  if (i < v->cnt) {
+    n = v->cnt+l;
+    /*                    __l__
+    **  ABCDEFGH       ABC-----DEFGH
+    **  |  |    |      |  |    |    |
+    **  0  i    cnt    0  i   i+l   cnt+l
+    */
+  }
+  else {
+    n = i+l;
+    /*                                           __l__
+    **  ABCDEFGH........          ABCDEFGH............
+    **  |       |     |           |             |     |
+    **  0       cnt   i           0             i     i+l
+    */
+  }
+  vecdbg("GAP cnt:%d i:%d l:%d n:%d",v->cnt,i,l,n);
+  if (!makeroom(v, n)) return 0;
+  if (i < v->cnt)  memmove(&v->vec[i+l],  &v->vec[i],  (v->cnt-i)*sizeof(val_t));
+  v->cnt = n;
+  return 1;
+}
+
+val_t *vec(val_t v)
+{
+  if (!valisvec(v)) return NULL;
+  return valtovec(v)->vec;
+}
+
+int vectype_2(val_t vv,int type)
+{ 
+  vec_t v;
+  if (!valisvec(vv)) return VEC_ISNULL;
+  v = valtovec(vv);
+  if (type >=0 ) v->typ = (type & 0x7F) | (v->typ & 0x80);
+  return (v->typ & 0x7F);
+}
+
+uint32_t vecsize_2(val_t vv, uint32_t n)
+{
+  vec_t v;
+  if (!valisvec(vv)) {errno = EINVAL; return 0;}
+  v = valtovec(vv);
+  if (n != VECNONDX && !makeroom(v,n)) return 0;
+  return v->sze;
+}
+
+uint32_t veccount_2(val_t vv, uint32_t n)
+{
+  uint32_t fst = 0;
+  vec_t v;
+
+  if (!valisvec(vv)) {errno = EINVAL; return 0;}
+  v = valtovec(vv);
+  if (vectype(vv) == VEC_ISQUEUE) fst = v->fst;
+
+  if (n != VECNONDX)  {
+    n += fst;
+    if (!makeroom(v, n)) return 0;
+    v->cnt = n;
+  }
+
+  return v->cnt - fst;
+}
+
+uint32_t vecset_(val_t vv, uint32_t i, val_t x)
+{
+  if (!valisvec(vv)) return ((errno=EINVAL),VECNONDX);
+  vec_t v = valtovec(vv);
   if (i == VECNONDX) i = v->cnt;
   if (!makeroom(v,i)) return VECNONDX;
   v->vec[i] = x;
@@ -71,12 +157,148 @@ uint32_t vec_set(vec_t v, uint32_t i, val_t x)
   return i;
 }
 
-val_t vec_get_2(vec_t v, uint32_t i)
+val_t vecget(val_t vv, uint32_t i)
 {
-  _vecdbg("v=%p",(void*)v);
-  if (!v || v->cnt == 0) {errno = EINVAL; return valnil; }
+  vec_t v;
+  if (!valisvec(vv) || (v = valtovec(vv))->cnt == 0) {errno = EINVAL; return valnil; }
   if (i == VECNONDX) i = v->cnt - 1; // Get the last element
   if (i >= v->cnt) {errno = ERANGE; return valnil; }
   return v->vec[i];
 }
 
+val_t vecdel_3(val_t vv, uint32_t i, uint32_t j)
+{
+  uint32_t l;
+  vec_t v;
+  if (!valisvec(vv) || (v = valtovec(vv))->cnt == 0) {errno = EINVAL; return valnil; }
+  vec_dbg("i = %d, j = %d, cnt = %d, sze = %d",i,j,v->cnt,v->sze);
+  if (i == VECNONDX) { i = v->cnt-1; j = i; }
+  else if (j == VECNONDX) j = i;
+  if (i >= v->cnt || j<i) { errno = EINVAL; return valnil; }
+  if (j >= v->cnt) {j = v->cnt-1;}
+  if (j+1 >= v->cnt) {
+    v->cnt = i; // Just drop last elements
+  }
+  else {
+    l = (j-i)+1;
+    // ```
+    //       __l__                                
+    //    ABCdefghIJKLM              ABCIJKLM     
+    //    |  |    |    |             |  |    |    
+    //    0  i   i+l   cnt           0  i    cnt-l  
+    // ```
+    memmove(&v->vec[i] , &v->vec[i+l],  (v->cnt-(i+l)) * sizeof(val_t));
+    v->cnt -= l;
+  }
+  vec_dbg("i = %d, j = %d, cnt = %d, sze = %d",i,j,v->cnt,v->sze);
+  return(v->cnt? v->vec[v->cnt - 1] : valnil);
+}
+
+val_t vecdrop_2(val_t vv, uint32_t n) {
+  vec_t v;
+  uint32_t cnt;
+  if (!valisvec(vv)) {errno = EINVAL; return valnil; }
+  v = valtovec(vv);
+  cnt = veccount(vv);
+  if (n == 0 || cnt == 0) return ((errno=ERANGE),valnil);
+  if (n >= cnt) v->cnt = 0;
+  else v->cnt -= n;
+  return(v->vec[v->cnt]);
+}
+
+uint32_t vecpush_(val_t vv, val_t x) 
+{ 
+    if (!valisvec(vv)) return ((errno=EINVAL),VECNONDX);
+    vectype(vv, VEC_ISSTACK); 
+    return vecadd(vv,x); 
+}
+
+uint32_t vecenq_(val_t vv, val_t x)
+{
+  vec_t v;
+  if (!valisvec(vv)) {errno = EINVAL; return VECNONDX; }
+  vectype(vv,VEC_ISQUEUE);
+  v = valtovec(vv);
+  if ((v->fst > (v->cnt / 2)) && (v->cnt >= v->sze)) {
+    memmove(v->vec, &v->vec[v->fst] , (v->cnt - v->fst) * sizeof(val_t));
+    v->cnt -= v->fst;
+    v->fst = 0;
+  }
+  return vecadd(vv,x); 
+}
+
+// Deque (get the next element and removes it from the que)
+val_t vecdeq_2(val_t vv, uint32_t n) 
+{
+  val_t x;
+  vec_t v;
+  if (!valisvec(vv)) {errno = EINVAL; return valnil; }
+  if (veccount(vv) == 0) return valnil;
+  if (vectype(vv) != VEC_ISQUEUE) { errno = EINVAL; return valnil; }
+  v = valtovec(vv);
+  if (v->cnt < (v->fst + n) ) {
+    v->fst = v->cnt = 0; v->typ = VEC_ISVEC;
+    x = valnil;
+  }
+  else {
+    v->fst += n;
+    x = v->vec[v->fst-1];
+    if (v->fst >= v->cnt) {
+      v->fst = 0; v->cnt = 0; v->typ = VEC_ISVEC;
+    }
+  }
+  return x;
+}
+
+val_t vechead(val_t vv)
+{
+  vec_t v;
+  if (!valisvec(vv)) { errno = EINVAL; return valnil; }
+  v = valtovec(vv);
+  if (v->typ != VEC_ISQUEUE) { errno = EINVAL; return valnil; }
+  if (v->cnt <= v->fst) {return valnil;}
+  return v->vec[v->fst];   
+}
+
+val_t vectail(val_t vv)
+{
+  vec_t v;
+  if (!valisvec(vv)) {errno = EINVAL; return valnil; }
+  v = valtovec(vv);
+  if (v->typ != VEC_ISQUEUE) { errno = EINVAL; return valnil; }
+  if (v->cnt <= v->fst) {return valnil;}
+  return v->vec[v->cnt-1];   
+}
+
+// val_t vecfirst(vec_t v)
+// {
+//   uint32_t n;
+//   n = veccount(v);
+//   if (n == 0) {return valnil;}
+//   if (v->typ == VEC_ISQUEUE) return v->vec[v->fst];
+//   return v->vec[n-1];   
+// }
+
+val_t vecown(val_t vv)
+{
+  vec_t v;
+  if (!valisvec(vv)) { errno=EINVAL; return  valnil; }
+  v = valtovec(vv);
+  if (v->typ & VEC_ISOWNED) {errno = ERANGE; return vv; }
+  v->typ |= VEC_ISOWNED;
+  vv.v |= 1;
+  return vv;
+}
+
+
+uint32_t vecfirst(val_t v);
+uint32_t veclast(val_t v);
+uint32_t vecnext(val_t v);
+uint32_t vecprev(val_t v);
+uint32_t vec_cur_2(val_t v, uint32_t i);
+
+uint32_t vecindex(val_t v,uint32_t ndx)
+{
+
+  
+}
